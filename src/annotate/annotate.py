16e347fd91a97b3227cf29420ed4214ee83f13a1
emmon <20226124+ecroot@users.noreply.github.com>
2024-09-06 14:24:49 +0100

    Intermediate commit: restructuring and adding helper files for eventual packaging

--- src/annotate/annotate.py
+++ src/annotate/annotate.py
@@ -537,6 +537,31 @@ def add_annotation_column(df, data_list, column_name):
     return df
 
 
+def format_output_columns(df, orig_col_names):
+    """
+    For the eventual output, drop irrelevant columns from the dataframe, and rename others to original names
+    :param df: dataframe of input values and annotations
+    :param orig_col_names: list of the original column names
+    :return: formatted dataframe of values and annotations
+    """
+    # add the names of the new columns to the names of the original columns
+    for col in ['gene chrom', 'gene start', 'gene end', 'tChr', 'tStart', 'tEnd', 'Centromere',
+                'region-telomere_distance', 'repeats', 'fragile_sites',
+                'genes', 'gene_lengths', 'g-t_distance', 'gene_positions']:
+        orig_col_names.append(col)
+
+    # rename the columns accordingly
+    df.columns = orig_col_names
+
+    # drop the telomere and centromere columns that aren't needed in the final output
+    df = df.drop(columns=['tChr', 'tStart', 'tEnd', 'Centromere'])
+
+    # remove nans and set values to strings to prevent trailing .0s
+    df.fillna('', inplace=True)
+    df = df.astype(str)
+
+    return df
+
 # def find_overlapping_features_for_dysgu_output(dysgu_data):
 #     """
 #     annotate each of the regions/boundaries in the dysgu output with features that overlap them
@@ -637,6 +662,7 @@ def add_annotation_column(df, data_list, column_name):
 #     final_df = pandas.concat(vars_by_chrom, ignore_index=True)
 #
 #     return final_df
+
 
 def find_overlapping_features(regions, rep_regions, rep_df, gene_regions, gene_df, fs_regions, fs_df):
     """"
@@ -812,6 +838,44 @@ def calculate_gene_lengths(df):
     """
     df['gene_length'] = df['end'] - df['start']
     return df
+
+
+def annotate_gene_location_data(infile, outfile, colname):
+    """
+    read in a file containing chr:start-end formatted data
+    :param infile: full path to input file
+    :param outfile: full path to output file
+    :param str colname: name of column containing the relevant data
+    :return:
+    """
+    # create the output file
+    with pandas.ExcelWriter(outfile) as writer:
+        # read in the input file
+        infile = pandas.read_excel(infile, sheet_name=None)
+        # for each sheet in the input file (probably only one for each in this case)
+        for sname, s in infile.items():
+            # if the sheet is not empty
+            if not s.empty:
+                # store the original column names to be able to add them back in later
+                orig_columns = [col.strip() for col in s.columns]
+
+                # convert the positions into separate columns for compatibility with the rest of the pipeline
+                res = get_separate_chr_pos(s, colname)
+                # res.to_csv(os.path.join(outdir, csvoutname), sep='\t', index=False)
+                # add the relevant annotations
+
+                # calculate the distance between the region and the nearest telomere
+                res = calculate_distances_to_telomeres(res)
+                # print(res.head())
+
+                # annotate the genes, fragile sites, repeats and gene sizes
+                regions_df = annotate_overlaps(res)
+
+                # reformat the final df to match the input
+                final = format_output_columns(regions_df, orig_columns)
+
+                # write output file
+                final.to_excel(writer, sheet_name=sname, index=False)
 
 
 def calculate_distances_to_telomeres(df, keep_int=False, isgene=False):
