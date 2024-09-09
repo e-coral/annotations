f715de6f576ac0c55a4d157455e0fd00a183336e
emmon <20226124+ecroot@users.noreply.github.com>
2024-09-09 12:16:15 +0100

    Intermediate commit: restructuring and adding helper files for eventual packaging

--- example_annotation_scripts/annotate_captureC_spreadsheets.py
+++ example_annotation_scripts/annotate_captureC_spreadsheets.py
@@ -7,30 +7,6 @@ outdir = (Path(__file__).parent.parent / 'example_output_files').resolve()
 indir = (Path(__file__).parent.parent / 'example_input_files').resolve()
 
 
-# def format_columns(df, orig_col_names):
-#     """
-#     Drop irrelevant columns from the dataframe, and rename to original names
-#     :param df: dataframe of input values and annotations
-#     :param orig_col_names: list of the original column names
-#     :return: formatted dataframe of values and annotations
-#     """
-#     # add the names of the new columns to the names of the original columns
-#     for col in ['tChr', 'tStart', 'tEnd', 'Centromere', 'region-telomere_distance', 'repeats', 'fragile_sites',
-#                 'genes', 'gene_lengths', 'g-t_distance']:
-#         orig_col_names.append(col)
-#
-#     # rename the columns accordingly
-#     df.columns = orig_col_names
-#
-#     # drop the telomere and centromere columns that aren't needed in the final output
-#     df = df.drop(columns=['tChr', 'tStart', 'tEnd', 'Centromere'])
-#
-#     # set values to strings to prevent trailing .0s
-#     df = df.astype(str)
-#
-#     return df
-#
-
 def annotate_captureC_spreadsheet():
     """
     For all the captureC spreadsheets, read in the data, annotate it, and return the annotated file
